0d111362fd3da1106f5527877ce50925337ece44
Tapas Sahoo <tapasbca2015@gmail.com>
2025-02-09 19:12:48 +0530

    express setup and routing

---
+++ server(node).js
@@ -0,0 +1,66 @@
+const http = require('http');
+const connection = require('./db')
+const PORT = 5000;
+
+//Insert data into table
+const insertUser = (name, email) => {
+    const sql = `INSERT INTO users (name,email) VALUES (?,?)`;
+    connection.query(sql, [name, email], (err, result) => {
+        if (err) {
+            console.error('Insert Failed', err);
+        } else {
+            console.log('User Added Successfully', result.insertId);
+        }
+    })
+}
+
+//Fetch data into table
+const fetchUsers = () => {
+    const sql = `SELECT * FROM users LIMIT 2 OFFSET 2`;
+    connection.query(sql, (err, result) => {
+        if (err) {
+            console.error('Fetch Failed', err);
+        } else {
+            console.log('Users List', result);
+        }
+    })
+}
+
+//Update user email
+const updateUser = (id, newMail) => {
+    const sql = `UPDATE users SET email = ? WHERE id = ?`;
+    connection.query(sql, [newMail, id], (err, result) => {
+        if (err) {
+            console.error('Update Failed', err);
+        } else {
+            console.log('User updated successfully');
+        }
+    })
+}
+
+//Delete a user
+const deleteUser = (id) => {
+    const sql = `DELETE FROM users WHERE id = ?`;
+    connection.query(sql, [id], (err, result) => {
+        if (err) {
+            console.error('Delete Failed', err);
+        } else {
+            console.log('User deleted successfully');
+        }
+    })
+}
+
+// insertUser('ABC', 'abc@gmail.com');
+// updateUser(3, "abc@gmail.com");
+// deleteUser(1);
+fetchUsers();
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'content-type': 'text/plain' });
+    res.end('Server is running');
+});
+
+//Server Listen
+server.listen(PORT, () => {
+    console.log(`Server running on port http://localhost:${PORT}`);
+})
--- server.js
+++ server.js
@@ -1,67 +1,13 @@
-const http = require('http');
-const connection = require('./db')
-const PORT = 5000;
+var express = require("express");
+var app = express();
 
-//Insert data into table
-const insertUser = (name, email) => {
-    const sql = `INSERT INTO users (name,email) VALUES (?,?)`;
-    connection.query(sql, [name, email], (err, result) => {
-        if (err) {
-            console.error('Insert Failed', err);
-        } else {
-            console.log('User Added Successfully', result.insertId);
-        }
-    })
-}
-
-//Fetch data into table
-const fetchUsers = () => {
-    const sql = `SELECT * FROM users`;
-    connection.query(sql, (err, result) => {
-        if (err) {
-            console.error('Fetch Failed', err);
-        } else {
-            console.log('Users List', result);
-        }
-    })
-}
-
-//Update user email
-const updateUser = (id, newMail) => {
-    const sql = `UPDATE users SET email = ? WHERE id = ?`;
-    connection.query(sql, [newMail, id], (err, result) => {
-        if (err) {
-            console.error('Update Failed', err);
-        } else {
-            console.log('User updated successfully');
-        }
-    })
-}
-
-//Delete a user
-const deleteUser = (id) => {
-    const sql = `DELETE FROM users WHERE id = ?`;
-    connection.query(sql, [id], (err, result) => {
-        if (err) {
-            console.error('Delete Failed', err);
-        } else {
-            console.log('User deleted successfully');
-        }
-    })
-}
-
-
-// insertUser('John doe', 'johndoe@gmail.com');
-// updateUser(1, "abc@gmail.com");
-deleteUser(1);
-fetchUsers();
-
-const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'content-type': 'text/plain' });
-    res.end('Server is running');
+// console.log(app);
+app.get("/", (req, res) => {
+    res.send("Homepage");
 });
 
-//Server Listen
-server.listen(PORT, () => {
-    console.log(`Server running on port http://localhost:${PORT}`);
-})
+app.get("/contact/:id?", (req, res) => {
+    res.send(`Contact us ${req?.params?.id ?? ''}`);
+});
+
+app.listen(3000, () => console.log('Server running on port : 3000'));
